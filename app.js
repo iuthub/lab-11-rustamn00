0f978255e26807bacb6c944daff8ad6c38957ec7
asus <aaa@aaa.aaa>
2020-04-28 18:10:43 +0500

    finished?

---
+++ app.js
@@ -0,0 +1,58 @@
+window.onload = function () {
+
+    let calculator = document.querySelector('.calculator')
+    let keys = calculator.querySelector('.calculator__keys')
+    const display = document.querySelector('.calculator__display')
+
+    keys.addEventListener('click', function(e){
+    	if (e.target.matches('button')) {
+    		let key = e.target
+    		let action = key.dataset.action
+    		
+    		if (!action) {
+    			console.log('number key!')
+    		}
+
+    		if (action === 'add' ||
+    			action === 'subtract' ||
+    			action === 'multiply' ||
+    			action === 'divide') { console.log('operator key!') }
+    		
+    		if (action === 'decimal') {
+    			console.log('decimal key!')
+    		}
+    		if (action === 'clear') {
+    			console.log('clear key!')
+    		}
+    		if (action === 'calculate') {
+    			console.log('equal key!')
+    		}
+    	}
+
+
+    })
+
+    keys.addEventListener('click', e => {
+    	if (e.target.matches('button')) {
+    		const key = e.target
+    		const action = key.dataset.action
+    		const keyContent = key.textContent
+    		const displayedNum = display.textContent
+
+    		if (!action) {
+    			if (displayedNum === '0') {
+    				display.textContent = keyContent
+    			}
+    		}
+    		if (!action) {
+    			if (displayedNum === '0') {
+    				display.textContent = keyContent
+    			} else {
+    				display.textContent = displayedNum + keyContent
+    			}
+    		}
+    		
+    	}
+    })
+
+}
